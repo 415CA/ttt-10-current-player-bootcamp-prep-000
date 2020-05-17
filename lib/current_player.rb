0707476d9bc18f80e150d84af264212f51708df8
Chris Allen <chrisallen415@gmail.com>
2020-05-17 15:49:08 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,14 @@
+def turn_count(board)
+  counter = 0
+  board.each do |item|
+    if item == "X" || item == "O"
+      counter += 1
+    end
+  counter
+end
+
+def current_player(board)
+  current_count = turn_count(board)
+  if current_count % 2 == 0 ? "X" : "O"
+  end
+end
